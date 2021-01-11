53ac604a42f6fd836682a7bac1dfd3aa17a652d3
Oskar Gawlak <oskar.gawlak@gmail.com>
2021-01-12 00:19:40 +0100

    Revert "feat: add ApolloProvider as a root component [CU-c91zak]"

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
+import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ApolloProvider from "./ApolloProvider";
 
-ReactDOM.render(ApolloProvider, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
