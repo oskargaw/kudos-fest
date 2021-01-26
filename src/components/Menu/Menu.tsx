2ec26ef0de2c69aba1e502dc8e69c85716188bf5
Olga Czarnecka <olga.czarnecka@lunarlogic.io>
2021-01-26 22:01:12 +0100

    feat: toggle nav tabs depending of user being logged in [CU-d0ye7w]

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,14 +1,26 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import { AuthContext } from "../../context/authContext";
+
 const Menu = () => {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <div>
       <Link to="/">Home </Link>
       <Link to="/give-kudos">GiveKudos </Link>
       <Link to="/my-kudoses">MyKudoses </Link>
-      <Link to="/login">Login </Link>
-      <Link to="/register">Register </Link>
-      <Link to="/">Logout </Link>
+      {user ? (
+        <Link to="/login" onClick={logout}>
+          Logout
+        </Link>
+      ) : (
+        <>
+          <Link to="/login">Login </Link>
+          <Link to="/register">Register </Link>
+        </>
+      )}
     </div>
   );
 };
